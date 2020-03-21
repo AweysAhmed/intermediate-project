21bcd46cfa45de8b5b51c95d8eb176f210f716c6
Aweys Ahmed <aweysahmed@Aweyss-MacBook-Pro.local>
2020-03-21 01:20:52 -0400

    modified clearance session controller to allow the use of username or email

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# using controller to add :username to controller
+class UsersController < Clearance::UsersController
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end

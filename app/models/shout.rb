76df0531657e915527e089762aba7401c3b3e21e
Aweys Ahmed <aweysahmed@Aweyss-MacBook-Pro.local>
2020-04-06 23:15:53 -0400

    added shout types

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -3,8 +3,8 @@
 # shout model
 class Shout < ApplicationRecord
   belongs_to :user
+  belongs_to :content, polymorphic: true
 
-  validates :body, presence: true, length: { in: 1..144 }
   validates :user, presence: true
 
   default_scope { order(created_at: :desc) }

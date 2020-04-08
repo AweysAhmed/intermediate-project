ca338e5b39503807b99ac0f673a9ac58aacf1265
Aweys Ahmed <aweysahmed@Aweyss-MacBook-Pro.local>
2020-04-08 10:29:49 -0400

    adding paperclip and imagemagick

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_030409) do
+ActiveRecord::Schema.define(version: 2020_04_08_135733) do
+
+  create_table "photo_shouts", force: :cascade do |t|
+    t.string "image_file_name", null: false
+    t.string "image_content_type", null: false
+    t.bigint "image_file_size", null: false
+    t.datetime "image_updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "shouts", force: :cascade do |t|
     t.integer "user_id", null: false
